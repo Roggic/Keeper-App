2686a298fbad907b864bb1b77bff1712b80c436b
Roggic <rogenrenon@gmail.com>
2023-11-26 17:54:22 +0300

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Footer() {
+    const year = new Date().getFullYear();
+    return (
+        <footer>
+            <p>Copyright ⓒ {year}</p>
+        </footer>
+    )
+}
+
+export default Footer
