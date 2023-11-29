7abe279a05137e6a5d4a5e436caf98e8a0be3604
Roggic <rogenrenon@gmail.com>
2023-11-29 22:27:08 +0300

    using map function to render array of notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
+import notes from './notes';
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      {notes.map(note => (
+        <Note
+          key={note.id}
+          title={note.title}
+          content={note.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  )
 }
 
 export default App
